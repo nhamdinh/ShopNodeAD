17b9cda2c09b822bf42644bb66a7837e872cf065
nham-nguyen <nham.nguyen@uptempo-global.com>
2024-05-03 10:27:49 +0700

    build

--- build/assets/SubmenuTrigger-34b58daa.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./mui-2493782d.js";const t=({className:e,onClick:i})=>s.jsx("button",{className:e||"",onClick:i,"aria-label":"Open submenu",children:s.jsx("i",{className:"icon icon-ellipsis-vertical-solid text-[24px]"})});export{t as S};
